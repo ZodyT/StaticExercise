e95584828dc59ececa36f9c790e09719cb926557
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 12:51:51 -0600

    Finished

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is your temperature in C or F?");
+            string temp = Console.ReadLine().ToLower();
+
+            if (temp == "c")
+            {
+                Console.WriteLine("What is your Celsius temperature?");
+                double C = Convert.ToDouble(Console.ReadLine());
+
+                var CFinal = TempConverter.CelsiusToFahrenheit(C);
+                Console.WriteLine($"Your tempature is {CFinal} in Fahrenheit.");
+            }
+            else if (temp == "f")
+            {
+                Console.WriteLine("What is your Fahrenheit temperature?");
+                double F = Convert.ToDouble(Console.ReadLine());
+
+                var FFinal = TempConverter.FahrenheitToCelsius(F);
+                Console.WriteLine($"Your tempature is {FFinal} in Celsius.");
+
+            }
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    class TempConverter
+    {
+
+        //The class should have at least 2 methods, one called FahrenheitToCelsius 
+        //that will require a parameter and return a double, the other CelsiusToFahrenheit
+        //which will also require a parameter and return a double.
+
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            return (Fahrenheit - 32) /1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 9) / 5 + 32;
+        }
+    }
+}
